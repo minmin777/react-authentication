b7ebb30ad94c85b1f29a246396870a92955dac62
Yasmine Ayad <yasmineayad89@gmail.com>
2020-12-15 20:54:49 -0500

    basic setup and routes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import Staff from './components/pages/Staff'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/staff" exact={true} component={Staff} />
+        </div>
+        
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
